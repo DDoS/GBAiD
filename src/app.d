a70edb0e55e6a16bbac1b7ce282768f941134f16
Aleksi Sapon <QcTechs@gmail.com>
2014-07-01 23:30:14 -0400

    Replace main with trying to run the actual emulator

--- src/app.d
+++ src/app.d
@@ -1,13 +1,11 @@
 import std.stdio;
-import gbaid.arm;
-import gbaid.memory;
+
+import gbaid.system;
 
 public void main(string[] args) {
 	if (args.length < 2) {
 		throw new Exception("Missing ROM path as first argument");
 	}
-	ARMProcessor processor = new ARMProcessor();
-	ROM rom = new ROM(args[1]);
-	writeln(rom.getInt(0));
-	processor.test();
+	GameBoyAdvance gba = new GameBoyAdvance(args[1]);
+	gba.start();
 }
