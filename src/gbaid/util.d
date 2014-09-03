2b6cd912e3c780ce88a6d412d7b69028f60caeec
Aleksi Sapon <QcTechs@gmail.com>
2014-09-02 21:11:09 -0400

    Implement a simple scheduler

--- src/gbaid/util.d
+++ src/gbaid/util.d
@@ -1,6 +1,13 @@
 module gbaid.util;
 
+import core.time;
+import core.thread;
+import core.sync.condition;
+
 import std.conv;
+import std.range;
+import std.algorithm;
+import std.container;
 
 public uint ucast(byte v) {
     return cast(uint) v & 0xFF;
@@ -41,6 +48,13 @@ public uint countBits(uint i) {
      return (i + (i >>> 4) & 0x0F0F0F0F) * 0x01010101 >>> 24;
 }
 
+public int sign(long v) {
+    if (v == 0) {
+        return 0;
+    }
+    return 1 - (v >>> 62 & 0b10);
+}
+
 template getSafe(T) {
     public T getSafe(T[] array, int index, T def) {
         if (index < 0 || index >= array.length) {
@@ -75,4 +89,173 @@ public string toString(char[] cs) {
         }
     }
     return to!string(cs[0 .. end]);
+}
+
+public class Scheduler {
+    alias TaskFunction = void delegate();
+    private Thread thread;
+    private Mutex mutex;
+    private Condition emptyCondition;
+    private Condition waitingCondition;
+    private shared bool running = false;
+    private int nextFreeID = 1;
+    private Task* first;
+
+    public this() {
+        mutex = new Mutex();
+        emptyCondition = new Condition(mutex);
+        waitingCondition = new Condition(mutex);
+        thread = new Thread(&run);
+        thread.name = "Scheduler";
+        running = true;
+        thread.start();
+    }
+
+    public ~this() {
+        if (running) {
+            shutdown();
+        }
+    }
+
+    public void shutdown() {
+        first = null;
+        running = false;
+        emptyCondition.notify();
+        waitingCondition.notify();
+    }
+
+    public int schedule(TickDuration scheduledTime, TaskFunction run) {
+        synchronized (this) {
+            // generate an ID
+            int id = nextFreeID++;
+            // create the task on the heap
+            Task* task = new Task(id, scheduledTime, run);
+            // insert the task in the chain according to it's priority
+            Task* previous = null;
+            Task* current = first;
+            // find the spot in the chain
+            while (current != null && *task >= *current) {
+                previous = current;
+                current = current.next;
+            }
+            // insert as first if there's no previous
+            if (previous == null) {
+                first = task;
+            } else {
+                previous.next = task;
+                task.previous = previous;
+            }
+            if (current != null) {
+                current.previous = task;
+                task.next = current;
+            }
+            // notify the thread
+            synchronized (mutex) {
+                // only notify the thread on waiting when the task was inserted at front
+                if (*first == *task) {
+                    waitingCondition.notify();
+                }
+                // always notify the thread on empty
+                emptyCondition.notify();
+            }
+            // return the ID for cancelling
+            return id;
+        }
+    }
+
+    public void cancel(int id) {
+        synchronized (this) {
+            // check if there's anything to cancel
+            if (first == null) {
+                return;
+            }
+            // remove the task if present
+            Task* previous = null;
+            Task* current = first;
+            // look for the task in the chain
+            while ((*current).id != id) {
+                if (current.next == null) {
+                    // not in chain
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            // keep track of the old first for later use
+            Task* oldFirst = first;
+            // remove from the correct spot
+            if (previous == null) {
+                first = current.next;
+                if (first != null) {
+                    first.previous = null;
+                }
+            } else {
+                previous.next = current.next;
+                if (current.next != null) {
+                    current.next.previous = previous;
+                }
+            }
+            // check if there's no remaining task or the first was removed
+            if (first == null || *first != *oldFirst) {
+                 // if so, notify the thread on waiting
+                synchronized (mutex) {
+                    waitingCondition.notify();
+                }
+            }
+        }
+    }
+
+    private void run() {
+        Task next;
+        Duration waitTime;
+        while (running) {
+            // wait until we get a task
+            while (first == null) {
+                synchronized (mutex) {
+                    emptyCondition.wait();
+                }
+                // check for shutdown on wake up
+                if (!running) {
+                    return;
+                }
+            }
+            // fetch the first task, but don't remove to allow cancelling
+            next = *first;
+            // wait until it's time to run it
+            waitTime = dur!"nsecs"(next.scheduledTime.nsecs() - TickDuration.currSystemTick().nsecs());
+            if (!waitTime.isNegative()) {
+                synchronized (mutex) {
+                    waitingCondition.wait(waitTime);
+                }
+            }
+            // make sure the task wasn't cancelled
+            if (first != null && next == *first) {
+                // check if we're past the scheduled time
+                if (TickDuration.currSystemTick() >= next.scheduledTime) {
+                    // remove and run the task
+                    first = first.next;
+                    if (first != null) {
+                        first.previous = null;
+                    }
+                    next.run();
+                }
+            }
+        }
+    }
+
+    private static struct Task {
+        private int id;
+        private TickDuration scheduledTime;
+        private TaskFunction run;
+        private Task* previous;
+        private Task* next;
+
+        public bool opEquals()(auto ref const Task other) const {
+            return id == other.id;
+        }
+
+        public int opCmp(ref const Task other) const {
+            return sign((scheduledTime - other.scheduledTime).nsecs());
+        }
+    }
 }
