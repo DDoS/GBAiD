cf9f171d25e3583d320406ee3bb8ecec94d1a901
Aleksi Sapon <QcTechs@gmail.com>
2014-10-11 21:50:49 -0400

    Update TODOs

--- src/app.d
+++ src/app.d
@@ -26,8 +26,9 @@ public void main(string[] args) {
 	gba.run();
 
 	// TODO:
-	//       fix graphic glitch in PKMN and LoZ intro
-	//       finish input (move to new module, read in IO registers)
+	//       fix graphic glitch in LoZ intro
+	//       finish input (move to new module, read in IO registers, IRQs)
+	//       implement basic saves (needed to quicken debugging)
 	//       investigate PKMN crash (when entering random encounter)
 	//       finish implementing bitmap modes in graphics
 	//       rewrite more of the graphics in x64
