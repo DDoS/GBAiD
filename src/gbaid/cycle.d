9bc336f6fd5e7745ae1689f65be2fa05ff7bd972
Aleksi Sapon <aleksi@sapon.ca>
2016-12-11 18:00:01 -0500

    Get the cycle sharer sort of working

--- src/gbaid/cycle.d
+++ src/gbaid/cycle.d
@@ -40,18 +40,17 @@ public template CycleSharer(uint numberOfSharers) if (numberOfSharers > 0 && num
                 // If this is the first sharer, wait until all others are also done
                 // For any other sharer, wait until the first sharer has reset the "done" flags
                 enum sharersMask = (1 << numberOfSharers) - 1;
-                enum barrierMask = id == 0 ? sharersMask : sharerBit;
-                enum barrierCondition = id == 0 ? sharersMask : sharerBit;
-                while (doneWithCycles.atomicOp!"&"(barrierMask) == barrierCondition) {
+                enum barrierCondition = id == 0 ? "!= sharersMask" : "& sharerBit";
+                while (mixin("doneWithCycles.atomicLoad!(MemoryOrder.raw) " ~ barrierCondition)) {
                     Thread.yield();
-                    //writeln("1 ", id, ' ', doneWithCycles.atomicLoad!(MemoryOrder.raw), ' ', barrierCondition);
+                    //writeln("1 ", id, ' ', doneWithCycles.atomicLoad!(MemoryOrder.raw));
                 }
                 // The first sharer takes care of distributing a new batch of cycles
                 static if (id == 0) {
                     // Wait if necessary for cycles to become available
                     while (availableCycles.atomicLoad!(MemoryOrder.raw) < cycleBatchSize) {
                         Thread.yield();
-                        //writeln("2");
+                        //writeln("2 ", availableCycles.atomicLoad!(MemoryOrder.raw));
                     }
                     // Take the cycles from the available ones
                     availableCycles.atomicOp!"-="(cycleBatchSize);
