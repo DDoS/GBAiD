e4535b19e446a3da2bd56def04733eabc06c5d8a
Aleksi Sapon <aleksi@sapon.ca>
2017-05-17 18:49:21 -0400

    More work on the RTC (reading and writing registers works)

--- src/gbaid/gba/rtc.d
+++ src/gbaid/gba/rtc.d
@@ -5,12 +5,12 @@ import gbaid.util;
 import gbaid.gba.gpio;
 
 private enum Register {
-    NONE = 0,
-    CONTROL = 1,
-    DATETIME = 2,
-    TIME = 3,
-    FORCE_RESET = 4,
-    FORCE_IRQ = 5
+    NONE,
+    CONTROL,
+    DATETIME,
+    TIME,
+    FORCE_RESET,
+    FORCE_IRQ
 }
 
 private enum Register[] REGISTER_INDICES = [
@@ -18,11 +18,12 @@ private enum Register[] REGISTER_INDICES = [
     Register.CONTROL, Register.NONE, Register.TIME, Register.NONE
 ];
 
-private enum int[] REGISTER_PARAMETER_COUNTS = [0, 1, 7, 3, 0, 0];
-
 private enum State {
-    READ_COMMAND, READ_PARAMETERS
+    WRITE_COMMAND, WRITE_PARAMETERS, READ_PARAMETERS
 }
+
+// In order: year, month, day, day of the week, hour, minutes and seconds
+private enum ubyte[] DATETIME_CLEARED_VALUES = [0, 1, 1, 0, 0, 0, 0];
 
 public struct Rtc {
     private bool selected = false;
@@ -30,9 +31,11 @@ public struct Rtc {
     private bool io = false;
     private ubyte bitBuffer = 0;
     private uint bufferIndex = 0;
-    private State state = State.READ_COMMAND;
-    private Register register = Register.NONE;
-    private bool readCommand = false;
+    private State state = State.WRITE_COMMAND;
+    private Register command = Register.NONE;
+    private uint parameterIndex = 0;
+    private ubyte controlRegister = 0;
+    private ubyte[7] datetimeRegisters = DATETIME_CLEARED_VALUES;
 
     @property public GpioChip chip() {
         GpioChip chip;
@@ -57,36 +60,130 @@ public struct Rtc {
         }
         // We read or write the IO pin on the rising edge
         if (!clock && value) {
-            bitBuffer |= io << bufferIndex;
-            bufferIndex += 1;
-            // We received a byte, so process it
-            if (bufferIndex == 8) {
-                processByte(bitBuffer);
-                bitBuffer = 0;
-                bufferIndex = 0;
+            final switch (state) with (State) {
+                case WRITE_COMMAND:
+                case WRITE_PARAMETERS: {
+                    // Read the IO pin into the bit buffer
+                    bitBuffer |= io << bufferIndex;
+                    bufferIndex += 1;
+                    // We received a byte, so process it
+                    if (bufferIndex == 8) {
+                        processInputByte(bitBuffer);
+                        // Clear the buffer for the next byte
+                        bitBuffer = 0;
+                        bufferIndex = 0;
+                    }
+                    break;
+                }
+                case READ_PARAMETERS: {
+                    // Get the next byte to output if the buffer is empty
+                    if (bufferIndex == 0) {
+                        bitBuffer = nextOutputByte();
+                        bufferIndex = 8;
+                    }
+                    // Write the next buffered bit to the IO pin
+                    io = (bitBuffer >>> (8 - bufferIndex)) & 0b1;
+                    bufferIndex -= 1;
+                    break;
+                }
             }
         }
         clock = value;
     }
 
-    private void processByte(ubyte b) {
+    private void processInputByte(ubyte input) {
         final switch (state) with (State) {
-            case READ_COMMAND: {
+            case WRITE_COMMAND: {
                 // The first nibble must be 6
-                if ((b & 0b1111) != 6) {
+                if ((input & 0b1111) != 6) {
                     break;
                 }
                 // Update the state accordingly
-                state = READ_PARAMETERS;
-                register = REGISTER_INDICES[b >>> 4 & 0b111];
-                readCommand = b >>> 7;
-                import std.stdio; writefln("%08b %s %s", bitBuffer, register, readCommand ? "read" : "write");
+                state =  input & 0x80 ? READ_PARAMETERS : WRITE_PARAMETERS;
+                command = REGISTER_INDICES[input >>> 4 & 0b111];
+                import std.stdio; writefln("%08b %s %s", input, command, state);
+                // Process commands that don't have any parameters
+                switch (command) with (Register) {
+                    case FORCE_RESET:
+                        // Clear all the registers
+                        controlRegister = 0;
+                        datetimeRegisters = DATETIME_CLEARED_VALUES;
+                        break;
+                    case FORCE_IRQ:
+                        // TODO: Trigger a GamePak interrupt
+                        break;
+                    default:
+                        // Get ready for receiving parameters for the other commands
+                        bitBuffer = 0;
+                        bufferIndex = 0;
+                        parameterIndex = 0;
+                }
                 break;
             }
-            case READ_PARAMETERS: {
-                import std.stdio; writefln("%08b", bitBuffer);
+            case WRITE_PARAMETERS: {
+                import std.stdio; writefln("%08b", input);
+                switch (command) with (Register) {
+                    case CONTROL:
+                        // Input one byte
+                        if (parameterIndex < 1) {
+                            controlRegister = input;
+                        }
+                        break;
+                    case DATETIME:
+                        // Input seven bytes
+                        if (parameterIndex < 7) {
+                            datetimeRegisters[parameterIndex] = input;
+                        }
+                        break;
+                    case TIME:
+                        // Input three bytes
+                        if (parameterIndex < 3) {
+                            datetimeRegisters[parameterIndex + 4] = input;
+                        }
+                        break;
+                    default:
+                        // No parameters or doesn't correspond to any register, ignore the write
+                }
+                // Increment for the next parameter
+                parameterIndex += 1;
+                break;
             }
+            case READ_PARAMETERS:
+                throw new Exception("Unexpected state when processing an input byte: READ_PARAMETERS");
         }
+    }
+
+    private ubyte nextOutputByte() {
+        if (state != State.READ_PARAMETERS) {
+            throw new Exception("Unexpected state when processing an output byte: WRITE_*");
+        }
+        ubyte output = 0xFF;
+        switch (command) with (Register) {
+            case CONTROL:
+                // Output one byte
+                if (parameterIndex < 1) {
+                    output = controlRegister;
+                }
+                break;
+            case DATETIME:
+                // Output seven bytes
+                if (parameterIndex < 7) {
+                    output = datetimeRegisters[parameterIndex];
+                }
+                break;
+            case TIME:
+                // Output three bytes
+                if (parameterIndex < 3) {
+                    output = datetimeRegisters[parameterIndex + 4];
+                }
+                break;
+            default:
+                // No parameters or doesn't correspond to any register
+        }
+        import std.stdio; writefln("%08b", output);
+        // Increment for the next parameter
+        parameterIndex += 1;
+        return output;
     }
 
     private bool readIo() {
@@ -106,9 +203,21 @@ public struct Rtc {
             // Clear the bit buffer when deselected and reset the state
             bitBuffer = 0;
             bufferIndex = 0;
-            state = State.READ_COMMAND;
+            state = State.WRITE_COMMAND;
+            // Not all register bits are used. Those should be 0. We can clear them here.
+            clearUnusedRegisterBits();
         }
         selected = value;
+    }
+
+    private void clearUnusedRegisterBits() {
+        controlRegister &= 0b01101010;
+        datetimeRegisters[1] &= 0b00011111;
+        datetimeRegisters[2] &= 0b00111111;
+        datetimeRegisters[3] &= 0b00000111;
+        datetimeRegisters[4] &= 0b01111111;
+        datetimeRegisters[5] &= 0b01111111;
+        datetimeRegisters[6] &= 0b01111111;
     }
 
     private bool readFloating() {
